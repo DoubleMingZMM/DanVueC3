9e9324bdd2b095cacd6a3b67456ba57d2b58ca6c
Daniel <1602268076@qq.com>
2019-07-23 17:53:02 +0800

    alter eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint-config-daniel',
     'plugin:vue/recommended'
   ],
+  // 自定义规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
